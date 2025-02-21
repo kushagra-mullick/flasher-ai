6fd613813827576fcdc70157a672fd71938cb30f
Kushagra Mullick <96918316+kushagra-mullick@users.noreply.github.com>
2025-02-21 20:59:45 +0100

    Commit from Bolt to GitHub

--- src/types.ts
+++ src/types.ts
@@ -3,32 +3,3 @@ export interface Card {
   front: string;
   back: string;
 }
-
-export interface SpacedRepetitionInfo {
-  interval: number;
-  easeFactor: number;
-  consecutiveCorrect: number;
-  nextReviewDate: Date;
-}
-
-export interface EnhancedCard extends Card {
-  spacedRepetition?: SpacedRepetitionInfo;
-  lastReviewed?: Date;
-  performance?: number[];
-}
-
-export interface StudySession {
-  id: number;
-  date: Date;
-  cards: Array<{
-    id: number;
-    wasCorrect: boolean;
-    timeSpent: number;
-  }>;
-}
-
-export interface StudyRecommendation {
-  cards: EnhancedCard[];
-  suggestedDuration: number;
-  focusAreas: string[];
-}
